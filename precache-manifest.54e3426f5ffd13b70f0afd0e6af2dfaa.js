b866ae7a15b32655d119873f4202672fbb398383
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2020-05-20 21:50:07 +0200

    Updates

---
+++ precache-manifest.54e3426f5ffd13b70f0afd0e6af2dfaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aee1575ea7f14bf4dcbda68783cd484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8c5e1118c6bdf3d5d3",
+    "url": "/static/css/main.108a8593.chunk.css"
+  },
+  {
+    "revision": "4e30ed76feee2a20ddf6",
+    "url": "/static/js/2.0cda9a02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0cda9a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8c5e1118c6bdf3d5d3",
+    "url": "/static/js/main.718a95f0.chunk.js"
+  },
+  {
+    "revision": "6b3301d87da13f5c0478",
+    "url": "/static/js/runtime-main.b941a61b.js"
+  }
+]);
